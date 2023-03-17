2bf5b12fe00a42df9c064f9fd70b0098ad67c20c
Bykie <ibeabuchi.chibuike95@gmail.com>
2023-03-17 07:18:31 +0000

    first commit

--- Exercise 5.js
+++ Exercise 5.js
@@ -2,7 +2,7 @@ function myClickFunction () {
     var element = document.getElementById('pageheading');
     element.firstChild.nodeValue = 'New Heading';
 
-    var element = document.getElementById('paragraph');
+   var element = document.getElementById('paragraph');
     element.firstChild.nodeValue = 'New Paragraphing Text';
 }
 
--- Exercise 6.js
+++ Exercise 6.js
@@ -3,6 +3,10 @@ function updateHeading () {
         element.firstChild.nodeValue = 'New Heading';
 }
 
+function updateHeading1 () {
+    var element = document.getElementById ('pageheading');
+        element.firstChild.nodeValue = 'Page Heading';
+}
 function updateParagraph () {
     var element = document.getElementById ('paragraph');
         element.firstChild.nodeValue = 'New Paragraph';
@@ -10,7 +14,10 @@ function updateParagraph () {
 
 function myLoadFunction () {
     var element = document.getElementById('pageheading');
-        element.addEventListener ('click', updateHeading);
+        element.addEventListener ('mouseenter', updateHeading);
+
+    var element = document.getElementById('pageheading');
+        element.addEventListener ('mouseleave', updateHeading1);
 
     var element = document.getElementById ('paragraph');
         element.addEventListener ('click', updateParagraph);
