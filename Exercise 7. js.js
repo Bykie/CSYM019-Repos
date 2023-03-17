caff895a921798f60c8cbf8d85f3f58531796ecb
Bykie <ibeabuchi.chibuike95@gmail.com>
2023-03-17 09:13:39 +0000

    end of week 4

--- Exercise 7. js.js
+++ Exercise 7. js.js
@@ -1,12 +1,17 @@
 function clickCommand () {
-    var element = document.getElementById ('Greatness');
-    alert(element.value);
+    var element = document.getElementById ('input');
+    var div = document.getElementById ('result');
+    div.firstChild.nodeValue = element.value;
 
 }
+
+// function myLoadFunction () {
+//     var element = document.getElementById ('mybutton');
+//         element.addEventListener ('click', clickCommand);
+// }
 
 function myLoadFunction () {
-    var element = document.getElementById ('mybutton');
-        element.addEventListener ('click', clickCommand);
+    var element = document.getElementById('input');
+        element.addEventListener('keyup', clickCommand);
 }
-
 document.addEventListener ('DOMContentLoaded', myLoadFunction);
